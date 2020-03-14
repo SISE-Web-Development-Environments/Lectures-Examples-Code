2423aa9212ccb6f49bdbe9b621cddfffefcbd655
shir frumerman <shirfr@post.bgu.ac.il>
2020-03-14 19:58:32 +0200

    Last lesson 1

---
+++ node.js/express1/basic.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+//This app starts a server and listens on port 3000 for connections. The app responds with “Hello World!” for requests to the root URL (/) or route. For every other path, it will respond with a 404 Not Found.
