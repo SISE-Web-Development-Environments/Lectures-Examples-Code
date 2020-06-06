cd1b59aec7e1a2257e6522dc5c3f07c57b87c2b6
shirulaf <shirfr@post.bgu.ac.il>
2020-06-06 21:29:09 +0300

    first commit

---
+++ Frontend/Lecture2/base-project copy/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
